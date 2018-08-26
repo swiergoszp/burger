4f1f1be04e3149a18bad892f893e0d30577599c1
Paul <swiergoszp@gmail.com>
2018-08-26 12:28:06 -0600

    works

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers (
+
+	id INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    burger_name VARCHAR(50) NOT NULL,
+    devoured BOOLEAN DEFAULT false
+
+);
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Scarpone');
+
+INSERT INTO burgers (burger_name) VALUES ('El Chilango');
+
+INSERT INTO burgers (burger_name) VALUES ('Royale');
